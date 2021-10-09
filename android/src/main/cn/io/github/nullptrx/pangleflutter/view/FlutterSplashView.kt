345d26b386d2c9150027a89f62c8420b573796de
chengfengpiaopiao <869010486@qq.com>
2021-10-09 14:33:49 +0800

    feat:修改闪屏页异常计时逻辑

--- android/src/main/cn/io/github/nullptrx/pangleflutter/view/FlutterSplashView.kt
+++ android/src/main/cn/io/github/nullptrx/pangleflutter/view/FlutterSplashView.kt
@@ -19,146 +19,146 @@ import io.flutter.plugin.platform.PlatformView
 import io.github.nullptrx.pangleflutter.util.asMap
 import io.github.nullptrx.pangleflutter.v2.TTAdSlotManager
 
-class FlutterSplashView(val context: Activity, messenger: BinaryMessenger, val id: Int, params: Map<String, Any?>) : PlatformView, MethodChannel.MethodCallHandler {
+class FlutterSplashView(
+    val context: Activity,
+    messenger: BinaryMessenger,
+    val id: Int,
+    params: Map<String, Any?>
+) : PlatformView, MethodChannel.MethodCallHandler {
 
-  private val methodChannel: MethodChannel = MethodChannel(messenger, "nullptrx.github.io/pangle_splashview_$id")
-  private val container: FrameLayout
-  private var mTTSplashAd: TTSplashAd? = null
-  private var hideSkipButton = false
-  private lateinit var countDownTimer: CountDownTimer
-  private var isCompleted = false;
+    private val methodChannel: MethodChannel =
+        MethodChannel(messenger, "nullptrx.github.io/pangle_splashview_$id")
+    private val container: FrameLayout
+    private var mTTSplashAd: TTSplashAd? = null
+    private var hideSkipButton = false
+    private lateinit var countDownTimer: CountDownTimer
 
-  override fun onFlutterViewDetached() {
-    destroy();
-  }
-
-  private fun destroy(){
-    if(countDownTimer != null){
-      countDownTimer.cancel();
+    override fun onFlutterViewDetached() {
+        destroy();
     }
-    isCompleted = false
-    TTMediationAdSdk.unregisterConfigCallback(mSettingConfigCallback)
-    container.removeAllViews();
-    if(mTTSplashAd != null){
-      mTTSplashAd!!.destroy();
+
+    private fun destroy() {
+        countDownTimer.cancel();
+        TTMediationAdSdk.unregisterConfigCallback(mSettingConfigCallback)
+        container.removeAllViews();
+        if (mTTSplashAd != null) {
+            mTTSplashAd!!.destroy();
+        }
+        methodChannel.setMethodCallHandler(null)
     }
-  }
 
-  init {
-    methodChannel.setMethodCallHandler(this)
-    container = FrameLayout(context)
-    loadAdWithCallback(params)
-  }
+    init {
+        methodChannel.setMethodCallHandler(this)
+        container = FrameLayout(context)
+        loadAdWithCallback(params)
+    }
 
-  private fun loadAdByParam(params: Map<String, Any?>) {
-    val slotId = params["slotId"] as? String
-    if (slotId != null) {
-      val isSupportDeepLink = params["isSupportDeepLink"] as? Boolean ?: true
-      val tolerateTimeout = params["tolerateTimeout"] as? Double ?: 3
-      val exceptionTimeout = tolerateTimeout.toDouble() + 1;
-      hideSkipButton = params["hideSkipButton"] as? Boolean ?: false
-      val imgArgs: Map<String, Int?> = params["imageSize"]?.asMap() ?: mapOf()
-      val w: Int = imgArgs["width"] ?: 1080
-      val h: Int = imgArgs["height"] ?: 1920
+    private fun loadAdByParam(params: Map<String, Any?>) {
+        val slotId = params["slotId"] as? String
+        if (slotId != null) {
+            val isSupportDeepLink = params["isSupportDeepLink"] as? Boolean ?: true
+            val tolerateTimeout = params["tolerateTimeout"] as? Double ?: 3
+            val exceptionTimeout = tolerateTimeout.toDouble() + 1;
+            hideSkipButton = params["hideSkipButton"] as? Boolean ?: false
+            val imgArgs: Map<String, Int?> = params["imageSize"]?.asMap() ?: mapOf()
+            val w: Int = imgArgs["width"] ?: 1080
+            val h: Int = imgArgs["height"] ?: 1920
 
-      mTTSplashAd = TTSplashAd(context, slotId)
-      mTTSplashAd!!.setTTAdSplashListener(object : TTSplashAdListener {
-        override fun onAdClicked() {
-          postMessage("onClick")
-        }
-
-        override fun onAdShow() {
-          postMessage("onShow")
-        }
-
-        override fun onAdShowFail(adError: AdError) {
-          postMessage("onError", mapOf("message" to adError.message, "code" to adError.code))
-        }
-
-        override fun onAdSkip() {
-          postMessage("onSkip")
-        }
-
-        override fun onAdDismiss() {
-          postMessage("onTimeOver")
-        }
-      })
-      //step3:创建开屏广告请求参数AdSlot,具体参数含义参考文档
-      val adSlot = TTAdSlotManager.getSplashAdSlot(w, h, isSupportDeepLink);
-      var ttNetworkRequestInfo: TTNetworkRequestInfo;
-      ttNetworkRequestInfo = PangleNetworkRequestInfo("5156773", "887562558");
-      //step4:请求广告，调用开屏广告异步请求接口，对请求回调的广告作渲染处理
-      mTTSplashAd!!.loadAd(adSlot, ttNetworkRequestInfo, object : TTSplashAdLoadCallback {
-        override fun onSplashAdLoadFail(adError: AdError) {
-          postMessage(
-            "onError",
-            mapOf(
-              "message" to adError.message + " adMessgaeDetail=" + mTTSplashAd!!.adLoadInfoList,
-              "code" to adError.code
-            )
-          )
-        }
-
-        override fun onSplashAdLoadSuccess() {
-          if (mTTSplashAd != null) {
-            mTTSplashAd!!.showAd(container)
-            countDownTimer = object : CountDownTimer((exceptionTimeout *  1000).toLong(),1000){
-              override fun onFinish() {
-                if(!isCompleted){
-                  postMessage("onTimeOver")
+            mTTSplashAd = TTSplashAd(context, slotId)
+            mTTSplashAd!!.setTTAdSplashListener(object : TTSplashAdListener {
+                override fun onAdClicked() {
+                    postMessage("onClick")
                 }
-              }
-              override fun onTick(millisUntilFinished: Long) {
-              }
+
+                override fun onAdShow() {
+                    postMessage("onShow")
+                }
+
+                override fun onAdShowFail(adError: AdError) {
+                    postMessage(
+                        "onError",
+                        mapOf("message" to adError.message, "code" to adError.code)
+                    )
+                }
+
+                override fun onAdSkip() {
+                    postMessage("onSkip")
+                }
+
+                override fun onAdDismiss() {
+                    postMessage("onTimeOver")
+                }
+            })
+            //step3:创建开屏广告请求参数AdSlot,具体参数含义参考文档
+            val adSlot = TTAdSlotManager.getSplashAdSlot(w, h, isSupportDeepLink);
+            var ttNetworkRequestInfo: TTNetworkRequestInfo;
+            ttNetworkRequestInfo = PangleNetworkRequestInfo("5156773", "887562558");
+            //step4:请求广告，调用开屏广告异步请求接口，对请求回调的广告作渲染处理
+            mTTSplashAd!!.loadAd(adSlot, ttNetworkRequestInfo, object : TTSplashAdLoadCallback {
+                override fun onSplashAdLoadFail(adError: AdError) {
+                    postMessage(
+                        "onError",
+                        mapOf(
+                            "message" to adError.message + " adMessgaeDetail=" + mTTSplashAd!!.adLoadInfoList,
+                            "code" to adError.code
+                        )
+                    )
+                }
+
+                override fun onSplashAdLoadSuccess() {
+                    if (mTTSplashAd != null) {
+                        mTTSplashAd!!.showAd(container)
+                    }
+                }
+
+                override fun onAdLoadTimeout() {
+                    postMessage("onTimeOver")
+                }
+            }, tolerateTimeout.toInt())
+          countDownTimer = object : CountDownTimer((exceptionTimeout *  1000).toLong(),1000){
+            override fun onFinish() {
+                postMessage("onTimeOver")
             }
-            countDownTimer.start();
+            override fun onTick(millisUntilFinished: Long) {
+            }
           }
+          countDownTimer.start();
         }
+    }
 
-        override fun onAdLoadTimeout() {
-          postMessage("onTimeOver")
+    private fun loadAdWithCallback(params: Map<String, Any?>) {
+        /**
+         * 判断当前是否存在config 配置 ，如果存在直接加载广告 ，如果不存在则注册config加载回调
+         */
+        if (TTMediationAdSdk.configLoadSuccess()) {
+            loadAdByParam(params)
+        } else {
+            TTMediationAdSdk.registerConfigCallback(mSettingConfigCallback) //不能使用内部类，否则在ondestory中无法移除该回调
         }
-      }, tolerateTimeout.toInt())
-      }
-  }
-
-  private fun loadAdWithCallback(params:  Map<String, Any?>) {
-    /**
-     * 判断当前是否存在config 配置 ，如果存在直接加载广告 ，如果不存在则注册config加载回调
-     */
-    if (TTMediationAdSdk.configLoadSuccess()) {
-      loadAdByParam(params)
-    } else {
-      TTMediationAdSdk.registerConfigCallback(mSettingConfigCallback) //不能使用内部类，否则在ondestory中无法移除该回调
     }
-  }
 
-  private val mSettingConfigCallback = TTSettingConfigCallback {
-    loadAdByParam(params)
-  }
-
-  override fun getView(): View {
-    return container
-  }
-
-  override fun dispose() {
-    methodChannel.setMethodCallHandler(null)
-    destroy();
-  }
-
-  override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-
-    when (call.method) {
-      else -> result.notImplemented()
+    private val mSettingConfigCallback = TTSettingConfigCallback {
+        loadAdByParam(params)
     }
-  }
 
-  private fun postMessage(method: String, arguments: Map<String, Any?> = mapOf()) {
-    isCompleted = true;
-    if(countDownTimer != null){
-      countDownTimer.cancel();
+    override fun getView(): View {
+        return container
     }
-    methodChannel.invokeMethod(method, arguments)
-  }
+
+    override fun dispose() {
+        destroy();
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+
+        when (call.method) {
+            else -> result.notImplemented()
+        }
+    }
+
+    private fun postMessage(method: String, arguments: Map<String, Any?> = mapOf()) {
+        countDownTimer.cancel();
+        methodChannel.invokeMethod(method, arguments)
+    }
 }
 
